1d00c9d38bba0d6f9d02184eb20d095e0fad715a
parth181195 <parthjansari@outlook.com>
2019-10-10 07:30:02 +0530

    addes step-3

--- simon_says/lib/main.dart
+++ simon_says/lib/main.dart
@@ -1,10 +1,32 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: MyFirstWidget(),
     ));
 
-class MyFirstWidget extends StatelessWidget {
+class MyFirstWidget extends StatefulWidget {
+  @override
+  _MyFirstWidgetState createState() => _MyFirstWidgetState();
+}
+
+class _MyFirstWidgetState extends State<MyFirstWidget> {
+  List<String> says = [
+    'thing 1',
+    'thing 2',
+    'thing 3',
+    'thing 4',
+    'thing 5',
+    'thing 6',
+    'thing 7',
+  ];
+  String get whatsays {
+    int randomInt = Random().nextInt(says.length);
+    print(randomInt);
+    return says[randomInt];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +35,28 @@ class MyFirstWidget extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'hello world',
-        ),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Simon Says',
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  whatsays,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              RaisedButton(
+                child: Text('New Says'),
+                onPressed: () {
+                  this.setState(() {});
+                },
+              )
+            ]),
       ),
     );
   }
